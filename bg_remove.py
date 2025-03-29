0868e85996bbcfc67882bd8515ad87937d790abc
rodrigomourapintodsl <rodrigo.pinto@desbravador.com.br>
2025-03-29 17:26:50 -0300

    teste1

--- bg_remove.py
+++ bg_remove.py
@@ -17,7 +17,7 @@ st.set_page_config(layout="wide", page_title="Image Background Remover")
 @st.cache_data
 def sqlteste():
     conn = st.connection("postgresql_servidor", type="sql")
-    df = conn.query("SELECT * FROM EMPRESA;", 
+    df = conn.query("SELECT EMPRESA.DSAPELIDO FROM EMPRESA;", 
                                      ttl=3600,
                                      show_spinner=None,
                                     #  params={idserie,dataini,datafim},
